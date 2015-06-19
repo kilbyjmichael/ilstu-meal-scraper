193e3c5e4bd8f6348fb961adbe906b90fea7f74f
Michael Kilby <kilbyjmichael@gmail.com>
2015-06-19 01:12:11 -0500

    Create html.html to store request html Made testing defs and main

--- ilstu_meal_grabber.py
+++ ilstu_meal_grabber.py
@@ -5,15 +5,12 @@ The requests are working now! I just need requests.sessions for cookies.
 
 >>> response = requests.post('http://dining.illinoisstate.edu/NetNutrition/123')
 >>> cookies = 'response.cookies'
-
 '''
 
 import requests
-import simplejson
 
 # Just in case we need a real refer url
 refer_url = 'http://dining.illinoisstate.edu/NetNutrition/123'
-TYPE = 'POST'
 
 # Use this stuff to request spesific buildings/places
 build_url = 'http://dining.illinoisstate.edu/NetNutrition/123/Unit/SelectUnitFromUnitsList'
@@ -27,26 +24,39 @@ meal_url = 'http://dining.illinoisstate.edu/NetNutrition/123/Menu/SelectMenu'
 meal_arg = 'menuOid='
 
 building_codes = {# these are the nums for the places
-                'waterson' : 1
-                'linkins'  : 16
-                'airport'  : 29
-                'bistro'   : 38}
+                'waterson' : '1',
+                'linkins'  : '16',
+                'airport'  : '29',
+                'bistro'   : '38'}
 
-waterson_places = {# places to get food
-                'homestyle' : 2
-                'freshbites': 3            
-                'yogurt'    : 4 # also inclueds ice cream/waffles
-                'asian'     : 5
-                'drinks'    : 6
-                'grill'     : 7
-                'pizza'     : 8
-                'cereal'    : 9
-                'salad'     : 10
-                'texmex'    : 11
-                'fruit'     : 12
-                'bakery'    : 13
-                'gluten'    : 14
-                'deli'      : 15}
+watterson_places = {# places to get food
+                'homestyle' : '2',
+                'freshbites': '3',          
+                'yogurt'    : '4', # also inclueds ice cream/waffles
+                'asian'     : '5',
+                'drinks'    : '6',
+                'grill'     : '7',
+                'pizza'     : '8',
+                'cereal'    : '9',
+                'salad'     : '10',
+                'texmex'    : '11',
+                'fruit'     : '12',
+                'bakery'    : '13',
+                'gluten'    : '14',
+                'deli'      : '15'}
 
-def request_food(url, param_type, number)
-    return requests.post(url, params=param_type + number)
+def request_menu(url, param_type, number, cookies):
+    return requests.post(url, data=param_type + number, cookies=cookies)
+
+def init_cookies():
+    return requests.post(refer_url).cookies
+    
+def test_request():
+    cookie = init_cookies()
+    data = request_menu(place_url, building_arg, watterson_places['drinks'], cookie)
+    print(data.content)
+
+def main():
+    test_request()
+    
+if __name__ == "__main__": main()
--- python_curl_testing.py
+++ python_curl_testing.py
@@ -1,14 +1,20 @@
 import requests
 
+'''Hypothesis: If you haven't visited all of the links (as if clicking), the server won't let you visit a farther one than you have clicked.'''
+
 # Get me some cookies
 # session = requests.Session()
 response = requests.post('http://dining.illinoisstate.edu/NetNutrition/123/')
 # cookies = session.cookies.get_dict()
 
-# Now that we have cookies, request stuff
-data = requests.post('http://dining.illinoisstate.edu/NetNutrition/123/Unit/SelectUnitFromUnitsList', data = 'unitOid=1', cookies = response.cookies)
+cookies = response.cookies
 
-print(data)
-print(data.content)
+# Now that we have cookies, request stuff
+data = requests.post('http://dining.illinoisstate.edu/NetNutrition/123/Unit/SelectUnitFromChildUnitsList', data = 'unitOid=6', cookies = cookies)
+
+# print(data)
+# print(data.content)
+
+print(data.content.find(b'Juice'))
 
 # Cookies now work.
